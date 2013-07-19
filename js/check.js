313ea3a228909596ff4a617a16df385f04511750
brodydittemore-eDip <dittemoremb@state.gov>
2013-07-19 16:17:15 -0400

    Uses access test

---
+++ js/check.js
@@ -0,0 +1,16 @@
+var check;
+
+function checkTest() {
+	if (check === undefined)
+		{
+			no();
+		}
+	else
+		{
+			yes();
+		}
+}
+	
+function no() {
+	alert("Adding leads is only available for State Department employees - which could be you! Go to www.usajobs! Be one of us!");
+}
---
+++ js/yes.js
@@ -0,0 +1,5 @@
+check = 0;
+
+function yes() {
+	alert("You work for State - add away!!");
+}
