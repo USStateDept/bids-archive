d0d9d2b327ae6830d4f626ceccc5f82a5b82360a
brodydittemore-eDip <dittemoremb@state.com>
2013-08-20 11:44:27 -0400

    Testing State only functionality (#31)
    Moved add a lead function into separate JS file

--- js/yes.js
+++ js/yes.js
@@ -1,5 +1,13 @@
 check = 0;
 
 function yes() {
-	alert("You work for State - add away!!");
+	Ext.Msg.show({
+					title : 'Add Your Lead',
+					msg : 'By clicking okay, you agree that any trade lead added to this system will be <b>unclassified</b>, no clearances will be necessary to view the information you add, and that you understand the rules and regulations regarding this site. All information you add will become publically available.<br><br>If you have any questions, please refer to our <a href="faqs.html" target="_blank">Frequently Asked Questions</a> and <a href="help.html" target="_blank">Help</a> pages.',
+					width : 300,
+					buttons : Ext.MessageBox.OK
+					//fn : checkTest
+				})
+				ga('send', 'event', 'Search_Panel', 'Add_Search_Panel', {'nonInteraction': 1});
+				myCallbackFunction;
 }
