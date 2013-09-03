914e61a9db5958aeff9c3313cd854fe4fed73082
brodydittemore-eDip <dittemoremb@state.com>
2013-09-03 19:01:14 -0400

    Initial commit for landing page (#91)

---
+++ js/landing.js
@@ -0,0 +1,270 @@
+window.onload = function() {
+	ieCheck();
+};
+
+function as(n) {
+	var themeUrl = "../ext-3.4.0/resources/css/xtheme-wireframe.css";
+	Ext.util.CSS.swapStyleSheet("theme", themeUrl);
+};
+
+Ext.onReady(function() {
+	var store, check;
+
+	var banks = [['African Development Bank'], ['Asian Development Bank'], ['Interamerican Development Bank'], ['Post Identified Project'], ['Washington Identified Project'], ['World Bank']]
+	var regions = [['Africa'], ['East Asia and the Pacific'], ['Europe'], ['Middle East and North Africa'], ['South and Central Asia'], ['Western Hemisphere']]
+	var arch = [['Archived'], ['In Procurement'], ['Pipeline']]
+	var sizes = [['0-25M'], ['25-50M'], ['50-100M'], ['>100M'], ['Unpublished']]
+	var sec = [['Ag and Environment'], ['Energy'], ['ICT'], ['Infrastructure'], ['Governance and Services'], ['Natural Resources']]
+
+	var btn_sideNav1, btn_sideNav2, btn_sideNav3, btn_sideNav4, btn_sideNav5, sideNavText;
+	
+	var leadTotalValue;
+	
+	store = new GeoExt.data.FeatureStore({
+		autoSave : true,
+		fields : [{
+			name : "Timestamp",
+			type : "string"
+		}, {
+			name : "Project_Funding_Source",
+			type : "string"
+		}, {
+			name : "Specific_Location",
+			type : "string"
+		}, {
+			name : "Country",
+			type : "string"
+		}, {
+			name : "DOS_Region",
+			type : "string"
+		}, {
+			name : "Project_Title",
+			type : "string"
+		}, {
+			name : "Project_Number",
+			type : "string"
+		}, {
+			name : "Link_To_Project",
+			type : "string"
+		}, {
+			name : "Sector",
+			type : "string"
+		}, {
+			name : "Keyword",
+			type : "string"
+		}, {
+			name : "Project_Size",
+			type : "string"
+		}, {
+			name : "Project_Announced",
+			type : "date",
+			dateFormat : "Y-m-d\\Z"
+		}, {
+			name : "Tender_Date",
+			type : "date",
+			dateFormat : "Y-m-d\\Z"
+		}, {
+			name : "Borrowing_Entity",
+			type : "string"
+		}, {
+			name : "Implementing_Entity",
+			type : "string"
+		}, {
+			name : "Project_POCs",
+			type : "string"
+		}, {
+			name : "Project_Description",
+			type : "string"
+		}, {
+			name : "Post_Comments",
+			type : "string"
+		}, {
+			name : "Submitting_Officer",
+			type : "string"
+		}, {
+			name : "Submitting_Officer_Contact",
+			type : "string"
+		}, {
+			name : "Source",
+			type : "string"
+		}, {
+			name : "US_Firm_Contact",
+			type : "string"
+		}, {
+			name : "US_Firm_Wins",
+			type : "string"
+		}, {
+			name : "Marker",
+			type : "string"
+		}, {
+			name : "Cleared",
+			type : "string"
+		}, {
+			name : "Status",
+			type : "string"
+		}, {
+			name : "fid",
+			type : "string"
+		}],
+		proxy : new GeoExt.data.ProtocolProxy({
+			protocol : new OpenLayers.Protocol.HTTP({
+				url : "http://" + domain + "/geoserver/opengeo/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=opengeo%3ADATATABLE&maxfeatures=230&outputformat=json&Filter=%3CFilter%3E%3COr%3E%3CPropertyIsEqualTo%3E%3CPropertyName%3EStatus%3C/PropertyName%3E%3CLiteral%3EIn%20Procurement%3C/Literal%3E%3C/PropertyIsEqualTo%3E%3CPropertyIsEqualTo%3E%3CPropertyName%3EStatus%3C/PropertyName%3E%3CLiteral%3EPipeline%3C/Literal%3E%3C/PropertyIsEqualTo%3E%3C/Or%3E%3C/Filter%3E",
+				format : new OpenLayers.Format.GeoJSON()
+			})
+		})//,
+		//autoLoad : true
+	});
+
+	store.load();
+	leadsSumValue = store.sum('Project_Size');
+	leadsWeekSumValue = store.sum('Project_Size');
+	leadsCount = store.getCount();
+	infCount = store.getCount();
+	ictCount = store.getCount();
+	ageCount = store.getCount();
+	gosCount = store.getCount();
+	narCount = store.getCount();
+	
+	var enteringHttpProxy = new Ext.data.HttpProxy({
+		url : 'servlet/Combo2',
+		method : 'GET'
+	});
+
+	var regionHttpProxy = new Ext.data.HttpProxy({
+		url : 'servlet/Combo2',
+		method : 'GET'
+	});
+
+	var sectorHttpProxy = new Ext.data.HttpProxy({
+		url : 'servlet/Combo2',
+		method : 'GET'
+	});
+
+	var fundingHttpProxy = new Ext.data.HttpProxy({
+		url : 'servlet/Combo2',
+		method : 'GET'
+	});
+
+	var sourceHttpProxy = new Ext.data.HttpProxy({
+		url : 'servlet/Combo2',
+		method : 'GET'
+	});
+
+	var enteringStore = new Ext.data.Store({
+		proxy : enteringHttpProxy,
+		baseParams : {
+			col : 'Submitting_Officer',
+			label : 'EnteringOfficer'
+		},
+
+		reader : new Ext.data.XmlReader({
+			record : 'Row',
+			id : 'ID'
+		}, ['EnteringOfficer'])
+	});
+
+	var regionStore = new Ext.data.Store({
+		proxy : regionHttpProxy,
+		baseParams : {
+			col : 'DOS_Region',
+			label : 'Region'
+		},
+
+		reader : new Ext.data.XmlReader({
+			record : 'Row',
+			id : 'ID'
+		}, ['Region'])
+	});
+
+	var sectorStore = new Ext.data.Store({
+		proxy : sectorHttpProxy,
+		baseParams : {
+			col : 'Sector',
+			label : 'Sector'
+		},
+
+		reader : new Ext.data.XmlReader({
+			record : 'Row',
+			id : 'ID'
+		}, ['Sector'])
+	});
+
+	var fundingStore = new Ext.data.Store({
+		proxy : fundingHttpProxy,
+		baseParams : {
+			col : 'Project_Funding_Source',
+			label : 'FundingSource'
+		},
+
+		reader : new Ext.data.XmlReader({
+			record : 'Row',
+			id : 'ID'
+		}, ['FundingSource'])
+	});
+
+	var sourceStore = new Ext.data.Store({
+		proxy : enteringHttpProxy,
+		baseParams : {
+			col : 'Source',
+			label : 'Source'
+		},
+
+		reader : new Ext.data.XmlReader({
+			record : 'Row',
+			id : 'ID'
+		}, ['Source'])
+	});
+
+	enteringStore.load();
+	sectorStore.load();
+	regionStore.load();
+	fundingStore.load();
+
+	var categorySelectedId;
+
+	// MAIN PANEL
+	var mainPanel = new Ext.FormPanel({
+		region : "center",
+		height : 500,
+		autoWidth : true,
+		html : '<div class="content"><div class="mainContent"><div style="text-align:center; padding-top: 20px"><div id="sideNavTextDiv"></div></div></div></div>'
+	});
+	
+	// FOOTER PANEL
+	var footerPanel = new Ext.FormPanel({
+		region : "south",
+		height : 70,
+		html : '<div id="footer" style="margin-top: 0px !important;">Sponsored by: The U.S. Department of State<br>State Department has compiled this information in order to help identify opportunities for U.S. businesses.  It is not intended to be complete and interested parties should not solely rely on the information provided herein, and neither the U.S. Government not its employees/contractors assume any legal liability for the accuracy, completeness, or  usefulness of any information or process disclosed. It is the sole responsibility of the user of the information to verify its accuracy.</div>'
+	});
+	
+	// SIDENAV PANEL
+	var sideNavPanel = new Ext.FormPanel({
+		frame : false,
+		region : 'center',
+		html : '<ul class="sideNav"><li id="sideNav1"><a href="javascript:displaySideNav1Text()">What is BIDS?</a></li><li id="sideNav2"><a href="javascript:displaySideNav2Text()">How many opportunities?</a></li><li id="sideNav3"><a href="javascript:displaySideNav3Text()">Who uses BIDS?</a></li><li id="sideNav4"><a href="javascript:displaySideNav4Text()">Getting started</a></li><li id="sideNav5"><a href="index.html">Map of leads</a></li></ul></div>'
+	});
+
+	// Creates the Layout
+	new Ext.Viewport({
+		layout : "fit",
+		hideBorders : true,
+		autoHeight: true,
+		autoScroll: false,
+		minWidth: 1000,
+		items : {
+			layout : "border",
+			items : [{
+				region : 'north',
+				html : '<div id="wrap"><div id="header"><div class="row" style="margin: 0px -12px 0px 7px;"><a class="logo" data-bind="click: showHome" href="#"/><img id="bidsLogo" alt="BIDS Logo" src="img/bidsLogo.png"></a><ul class="nav"><li><a href="mailto:BIDS-Mailbox@state.gov">Contact Us</a></li><li><a href="help.html">Help</a></li><li><a href="faqs.html">FAQs</a></li><li><a href="data.html">Data</a></li><li><a href="map.html">Leads Map</a></li><li><a href="index.html">Home</a></li></ul></div></div>',
+				height : 121,
+				maxWidth: 1200,
+				border : true
+			}, mainPanel, {
+				layout : 'border',
+				region : 'west',
+				width : 195,
+				items : [sideNavPanel]
+			}, footerPanel]
+		}
+	});
+});
