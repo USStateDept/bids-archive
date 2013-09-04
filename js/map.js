56e7548226ad057588c86de9e6882544cf2a031b
brodydittemore-eDip <dittemoremb@state.com>
2013-09-04 17:17:45 -0400

    Moving map button

--- js/map.js
+++ js/map.js
@@ -713,7 +713,7 @@ Ext.onReady(function() {
 			layout : "border",
 			items : [{
 				region : 'north',
-				html : '<div id="wrap"><div id="header"><div class="row" style="margin: 0px -12px 0px 7px;"><a class="logo" data-bind="click: showHome" href="#"/><img id="bidsLogo" alt="BIDS Logo" src="img/bidsLogo.png"></a><ul class="nav"><li><a href="mailto:BIDS-Mailbox@state.gov">Contact Us</a></li><li><a href="help.html">Help</a></li><li><a href="faqs.html">FAQs</a></li><li><a href="data.html">Data</a></li><li><a href="#">Map</a></li><li><a href="javascript:checkTest();">Add a Lead</a></li><li><a href="index.html">Home</a></li></ul></div></div>',
+				html : '<div id="wrap"><div id="header"><div class="row" style="margin: 0px -12px 0px 7px;"><a class="logo" data-bind="click: showHome" href="#"/><img id="bidsLogo" alt="BIDS Logo" src="img/bidsLogo.png"></a><ul class="nav"><li><a href="mailto:BIDS-Mailbox@state.gov">Contact Us</a></li><li><a href="help.html">Help</a></li><li><a href="faqs.html">FAQs</a></li><li><a href="data.html">Data</a></li><li><a href="javascript:checkTest();">Add a Lead</a></li><li><a href="#">Map</a></li><li><a href="index.html">Home</a></li></ul></div></div>',
 				height : 121,
 				border : true
 			}, mapPanel, {
