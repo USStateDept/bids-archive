06e8dc7f6267fc312762455c56784d1f0e2da0c9
brodydittemore-eDip <dittemoremb@state.com>
2013-12-05 09:56:59 -0500

    Analytics modifications

--- js/host.js
+++ js/host.js
@@ -1,3 +1,19 @@
 var host = location.host;
 var loc = window.location.pathname;
 var site = loc.substring(0, loc.lastIndexOf('/')) + '/';
+
+var gaSite = function() {
+	if (host === 'edipmaps.net') {
+		'edipmaps.net';
+	} else {
+		'state.gov';
+	}
+}
+
+var gaID = function() {
+	if (host === 'edipmaps.net') {
+		'UA-42151027-1';
+	} else {
+		'UA-42151027-2';
+	}
+}
