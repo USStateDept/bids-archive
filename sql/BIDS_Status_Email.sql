53d5cf90ad56f07dda2b9fbc799e1a5a0ea56067
brodydittemore-eDip <dittemoremb@state.com>
2014-02-18 12:56:02 -0500

    Updating SQL query for #53
    Pull contact method from Contact_Status column

--- sql/BIDS_Status_Email.sql
+++ sql/BIDS_Status_Email.sql
@@ -1,81 +1,56 @@
-/* THIS QUERY CREATES A LIST OF LEADS ADDED TO THE DATATABLE 90 DAYS OR MORE AGO.	*/
-/* IT IS THEN USED TO SEND EMAILS TO THE LEAD's SUBMITTING OFFICER, AND				*/
-/* UPDATES THE CONTACT_TIMESTAMP TO BE USED THE NEXT TIME.							*/
+/* Drop the tables of leads to contact if they exists							*/
+DROP TABLE IF EXISTS opengeo."BIDS_Status_Email_List";
 
-/* This is just a temp copy of the database for testing 							*/
-/*
-DROP TABLE IF EXISTS opengeo."DATATABLE_test";
+/* Selects records with a Contact_Timestamp of certain ages 						*/
+/* and creates a table with the selection in order to be used .						*/
+/* Contact_Status specifies how old the contact is and how it should be contacted			*/
+/* to send emails to these leads 									*/
+/* TO: FSO												*/
+/* FROM: BIDS-Mailbox											*/
+/* Allow options to link back to bids.state.gov to edit, or an archive button				*/
+
 SELECT *
-INTO opengeo."DATATABLE_test"
-FROM "DATATABLE";
-*/
+INTO opengeo."BIDS_Status_Email_List"
+FROM opengeo."DATATABLE"
+WHERE "DATATABLE"."Contact_Timestamp" >= ('now'::text::date - '90 days'::interval)
+	AND "DATATABLE"."Contact_Timestamp" <= ('now'::text::date - '89 days'::interval)
+	AND "DATATABLE"."Archived" = '0'
+	AND "DATATABLE"."Cleared" = '1';
+ALTER TABLE Opengeo."BIDS_Status_Email_List" ADD COLUMN Contact_Status integer;
 
-/* Drop the tables of leads to contact if they exists 								*/
-DROP TABLE IF EXISTS opengeo."BIDS_Status_Email_List_90day";
-DROP TABLE IF EXISTS opengeo."BIDS_Status_Email_List_140day";
-DROP TABLE IF EXISTS opengeo."BIDS_Status_Email_List_150day";
-DROP TABLE IF EXISTS opengeo."BIDS_Status_Email_List_151day";
-
-
-/* Selects records with a Contact_Timestamp of 90 days  	 						*/
-/* and creates a table with the selection in order to be used 						*/
-/* to send emails to these leads 													*/
-/* TO: FSO																			*/
-/* FROM: BIDS-Mailbox																*/
-/* Allow options to link back to bids.state.gov to edit, or an archive button		*/
+INSERT INTO opengeo."BIDS_Status_Email_List" 
 SELECT *
-INTO opengeo."BIDS_Status_Email_List_90day"
-FROM opengeo."DATATABLE_test"
-WHERE "DATATABLE_test"."Contact_Timestamp" > ('now'::text::date - '90 days'::interval)
-	AND "DATATABLE_test"."Contact_Timestamp" < ('now'::text::date - '89 days'::interval)
-	AND "DATATABLE_test"."Archived" = '0'
-	AND "DATATABLE_test"."Cleared" = '1';
-ALTER TABLE Opengeo."BIDS_Status_Email_List_90day" ADD COLUMN fid BIGSERIAL PRIMARY KEY;
+FROM opengeo."DATATABLE"
+WHERE "DATATABLE"."Contact_Timestamp" >= ('now'::text::date - '140 days'::interval)
+	AND "DATATABLE"."Contact_Timestamp" <= ('now'::text::date - '139 days'::interval)
+	AND "DATATABLE"."Archived" = '0'
+	AND "DATATABLE"."Cleared" = '1';
 
-
-/* Selects records with a Contact_Timestamp of 140 days  	 						*/
-/* and creates a table with the selection in order to be used 						*/
-/* to send emails to these leads 													*/
-/* TO: FSO																			*/
-/* FROM: BIDS-Mailbox																*/
-/* Allow options to link back to bids.state.gov to edit, or an archive button		*/
+INSERT INTO opengeo."BIDS_Status_Email_List" 
 SELECT *
-INTO opengeo."BIDS_Status_Email_List_140day"
-FROM opengeo."DATATABLE_test"
-WHERE "DATATABLE_test"."Contact_Timestamp" > ('now'::text::date - '140 days'::interval)
-	AND "DATATABLE_test"."Contact_Timestamp" < ('now'::text::date - '139 days'::interval)
-	AND "DATATABLE_test"."Archived" = '0'
-	AND "DATATABLE_test"."Cleared" = '1';
-ALTER TABLE Opengeo."BIDS_Status_Email_List_140day" ADD COLUMN fid BIGSERIAL PRIMARY KEY;
+FROM opengeo."DATATABLE"
+WHERE "DATATABLE"."Contact_Timestamp" >= ('now'::text::date - '150 days'::interval)
+	AND "DATATABLE"."Contact_Timestamp" <= ('now'::text::date - '149 days'::interval)
+	AND "DATATABLE"."Archived" = '0'
+	AND "DATATABLE"."Cleared" = '1';
 
-
-/* Selects records with a Contact_Timestamp of 150 days  	 						*/
-/* and creates a table with the selection in order to be used 						*/
-/* to send emails to these leads 													*/
-/* TO: BIDS-Mailbox																	*/
-/* FROM: BIDS-Mailbox																*/
-/* Allow options to link back to bids.state.gov to edit, or an archive button		*/
+INSERT INTO opengeo."BIDS_Status_Email_List" 
 SELECT *
-INTO opengeo."BIDS_Status_Email_List_150day"
-FROM opengeo."DATATABLE_test"
-WHERE "DATATABLE_test"."Contact_Timestamp" > ('now'::text::date - '150 days'::interval)
-	AND "DATATABLE_test"."Contact_Timestamp" < ('now'::text::date - '149 days'::interval)
-	AND "DATATABLE_test"."Archived" = '0'
-	AND "DATATABLE_test"."Cleared" = '1';
-ALTER TABLE Opengeo."BIDS_Status_Email_List_150day" ADD COLUMN fid BIGSERIAL PRIMARY KEY;
+FROM opengeo."DATATABLE"
+WHERE "DATATABLE"."Contact_Timestamp" >= ('now'::text::date - '151 days'::interval)
+	AND "DATATABLE"."Contact_Timestamp" <= ('now'::text::date - '150 days'::interval)
+	AND "DATATABLE"."Archived" = '0'
+	AND "DATATABLE"."Cleared" = '1';
 
-
-/* Selects records with a Contact_Timestamp of 151 days  	 						*/
-/* and creates a table with the selection in order to be used 						*/
-/* to send emails to these leads 													*/
-/* TO: FSO and BIDS-Mailbox															*/
-/* FROM: BIDS-Mailbox																*/
-/* Allow option to link back to bids.state.gov to edit								*/
-SELECT *
-INTO opengeo."BIDS_Status_Email_List_151day"
-FROM opengeo."DATATABLE_test"
-WHERE "DATATABLE_test"."Contact_Timestamp" > ('now'::text::date - '151 days'::interval)
-	AND "DATATABLE_test"."Contact_Timestamp" < ('now'::text::date - '150 days'::interval)
-	AND "DATATABLE_test"."Archived" = '0'
-	AND "DATATABLE_test"."Cleared" = '1';
-ALTER TABLE Opengeo."BIDS_Status_Email_List_151day" ADD COLUMN fid BIGSERIAL PRIMARY KEY;
+UPDATE "opengeo"."BIDS_Status_Email_List"
+SET Contact_Status = 
+	CASE 
+		WHEN "BIDS_Status_Email_List"."Contact_Timestamp" >= ('now'::text::date - '90 days'::interval) AND "BIDS_Status_Email_List"."Contact_Timestamp" <= ('now'::text::date - '89 days'::interval)
+			THEN 90
+		WHEN "BIDS_Status_Email_List"."Contact_Timestamp" >= ('now'::text::date - '140 days'::interval) AND "BIDS_Status_Email_List"."Contact_Timestamp" <= ('now'::text::date - '139 days'::interval)
+			THEN 140
+		WHEN "BIDS_Status_Email_List"."Contact_Timestamp" >= ('now'::text::date - '150 days'::interval) AND "BIDS_Status_Email_List"."Contact_Timestamp" <= ('now'::text::date - '149 days'::interval) 
+			THEN 150
+		WHEN "BIDS_Status_Email_List"."Contact_Timestamp" >= ('now'::text::date - '151 days'::interval) AND "BIDS_Status_Email_List"."Contact_Timestamp" <= ('now'::text::date - '150 days'::interval) 
+			THEN 151
+	END;
